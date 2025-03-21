d5903dfdb2ff62a8f194e7305b982f42deba7c57
NarenKarthikBM <85060104+NarenKarthikBM@users.noreply.github.com>
2025-03-21 21:52:22 +0530

    Update urls.py

--- events/urls.py
+++ events/urls.py
@@ -1,11 +1,7 @@
-from django.urls import path
-
-from . import views
-
-users_api_v1_urls = [
-    path(
-        "obtain-auth-token/",
-        views.UserObtainAuthTokenAPI().as_view(),
-        name="user-obtain-auth-token",
-    ),
+events_api_v1_urls = [
+    # path(
+    #     "obtain-auth-token/",
+    #     views.UserObtainAuthTokenAPI().as_view(),
+    #     name="user-obtain-auth-token",
+    # ),
 ]
