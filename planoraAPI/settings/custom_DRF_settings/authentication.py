ace24e616ced48ffc34aa8a1e5402b06b8fe7ca6
NarenKarthikBM <85060104+NarenKarthikBM@users.noreply.github.com>
2025-03-22 01:57:26 +0530

    Update authentication.py

--- planoraAPI/settings/custom_DRF_settings/authentication.py
+++ planoraAPI/settings/custom_DRF_settings/authentication.py
@@ -6,17 +6,12 @@ from users.models import UserAuthTokens
 
 class TokenAuthentication(authentication.BaseAuthentication):
     def authenticate(self, request):
-        auth_token, device_token = (
-            request.META.get("HTTP_AUTHORIZATION"),
-            request.META.get("HTTP_DEVICE_TOKEN"),
-        )
-        if not auth_token or not device_token:
+        auth_token = request.META.get("HTTP_AUTHORIZATION")
+        if not auth_token:
             return None
 
         try:
-            user_auth_token = UserAuthTokens.objects.get(
-                auth_token=auth_token, device_token=device_token
-            )
+            user_auth_token = UserAuthTokens.objects.get(auth_token=auth_token)
         except User.DoesNotExist:
             raise exceptions.AuthenticationFailed("No such user")
 
