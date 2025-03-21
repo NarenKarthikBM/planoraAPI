00e88e2edc41f6b2968f5c6f5c73eb2b2150ec27
NirmanJaiswal36 <nirmanjaiswal0369@gmail.com>
2025-03-22 03:53:21 +0530

    update

--- events/urls.py
+++ events/urls.py
@@ -1,7 +1,22 @@
+from django.urls import path
+
+from . import views
+from django.urls import path
+from utils.qr_generator import serve_qr
+
+urlpatterns = [
+    path('<int:event_id>/qr/', serve_qr, name="event_qr"),
+]
+
 events_api_v1_urls = [
-    # path(
-    #     "obtain-auth-token/",
-    #     views.UserObtainAuthTokenAPI().as_view(),
-    #     name="user-obtain-auth-token",
-    # ),
+    path(
+        "public-feed/",
+        views.EventsPublicFeedAPI().as_view(),
+        name="events-public-feed",
+    ),
+    path(
+        "personalised-feed/",
+        views.EventsFeedAPI().as_view(),
+        name="events-personalised-feed",
+    ),
 ]
